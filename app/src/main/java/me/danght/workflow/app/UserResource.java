909e7583bddc7ab690d4749762d48efea4436d9c
DangHT <dht925nerd@126.com>
2021-04-13 09:27:18 +0800

    为用户API添加运行时日志

--- app/src/main/java/me/danght/workflow/app/UserResource.java
+++ app/src/main/java/me/danght/workflow/app/UserResource.java
@@ -10,6 +10,7 @@ import me.danght.workflow.common.bo.ProcessInstanceBO;
 import me.danght.workflow.common.bo.TaskInstanceBO;
 import me.danght.workflow.common.constant.ProcessInstanceState;
 import org.apache.dubbo.config.annotation.DubboReference;
+import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
@@ -27,6 +28,8 @@ import java.util.Map;
 @Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
+    private static final Logger LOGGER = Logger.getLogger(UserResource.class);
+
     @Inject
     UserInfoService userInfoService;
 
@@ -38,30 +41,35 @@ public class UserResource {
 
     @GET
     public Iterable<UserInfoDO> getAllUsers() {
+        LOGGER.info("查询所有学生信息");
         return userInfoService.getAllUsers();
     }
 
     @GET
     @Path("/{id}")
     public UserInfoDTO getUser(@PathParam("id") String id) {
+        LOGGER.info("查询id=" + id + "的学生信息");
         return userInfoService.getUserById(id);
     }
 
     @GET
     @Path("queryApprovalList")
-    public List<ProcessDefinitionBO> queryApprovalList(){
+    public List<ProcessDefinitionBO> queryApprovalList() {
+        LOGGER.info("查询全部申请表");
         return clientProcessService.queryDefinitionList();
     }
 
     @GET
     @Path("queryUnTaskList")
     public List<TaskInstanceBO> queryUnTaskList(@QueryParam("uiId") String uiId){
+        LOGGER.info("查询全部未完成任务");
         return clientTaskService.selectUnCompletedTask(uiId,"0");
     }
 
     @GET
     @Path("queryUnObtainList")
     public List<TaskInstanceBO> queryUnObtainList(@QueryParam("uiId") String uiId){
+        LOGGER.info("查询全部未获取任务");
         UserInfoDTO userInfoDTO = userInfoService.getUserById(uiId);
         return clientTaskService.selectUnObtainTask(userInfoDTO.getGiId());
     }
@@ -69,6 +77,7 @@ public class UserResource {
     @GET
     @Path("queryProcessList")
     public List<ProcessInstanceBO> queryProcessList(@QueryParam("uiId") String uiId){
+        LOGGER.info("查询全部运转中流程");
         return clientProcessService.getProcessListByUserId(uiId);
     }
 
@@ -76,7 +85,12 @@ public class UserResource {
     @Path("obtainTask")
     @Consumes(MediaType.APPLICATION_JSON)
     public String obtainTask(@QueryParam("taskId") String taskId, @QueryParam("uiId") String uiId) {
-        clientTaskService.obtainTask(taskId,uiId);
+        LOGGER.info("用户(id=" + uiId + ")尝试获取任务(id=" + taskId + ")");
+        if (clientTaskService.obtainTask(taskId,uiId) != null) {
+            LOGGER.info("用户(id=" + uiId + ")获取任务(id=" + taskId + ")成功");
+        } else {
+            LOGGER.error("用户(id=" + uiId + ")获取任务(id=" + taskId + ")失败");
+        }
         return "1";
     }
 
@@ -87,7 +101,12 @@ public class UserResource {
             @QueryParam("pdId") String pdId,
             @QueryParam("tiId") String tiId,
             @QueryParam("userTaskNo") String userTaskNo) {
-        clientTaskService.completeTask(tiId,pdId,userTaskNo, new HashMap<>());
+        LOGGER.info("开始完成审阅任务(id=" + tiId + ")");
+        if (clientTaskService.completeTask(tiId,pdId,userTaskNo, new HashMap<>())) {
+            LOGGER.info("完成审阅任务(id=" + tiId + ")成功");
+        } else {
+            LOGGER.error("完成审阅任务(id=" + tiId + ")失败");
+        }
         return "1";
     }
 
@@ -95,7 +114,14 @@ public class UserResource {
     @Path("loginValidate")
     @Consumes(MediaType.APPLICATION_JSON)
     public UserInfoDTO loginValidate(@QueryParam("uiName") String uiName) {
-        return userInfoService.validateUser(uiName);
+        LOGGER.info("正在验证用户名: " + uiName);
+        UserInfoDTO user = userInfoService.validateUser(uiName);
+        if (user != null) {
+            LOGGER.info("验证用户名: " + uiName + " 成功");
+        } else {
+            LOGGER.error("验证用户名: " + uiName + " 失败");
+        }
+        return user;
     }
 
     @POST
@@ -106,9 +132,14 @@ public class UserResource {
             @QueryParam("tiId") String tiId,
             @QueryParam("userTaskNo") String userTaskNo,
             @QueryParam("businessDynamicAssignee01") String businessDynamicAssignee01) {
+        LOGGER.info("正在分配任务(id=" + tiId + "给用户(id=" + businessDynamicAssignee01 + ")");
         Map<String, Object> requiredData = new HashMap<>();
         requiredData.put("businessDynamicAssignee01",businessDynamicAssignee01);
-        clientTaskService.completeTask(tiId,pdId,userTaskNo,requiredData);
+        if (clientTaskService.completeTask(tiId,pdId,userTaskNo,requiredData)) {
+            LOGGER.info("分配任务(id=" + tiId + "给用户(id=" + businessDynamicAssignee01 + ")成功");
+        } else {
+            LOGGER.error("分配任务(id=" + tiId + "给用户(id=" + businessDynamicAssignee01 + ")失败");
+        }
         return "1";
     }
 
@@ -122,13 +153,24 @@ public class UserResource {
             @QueryParam("businessJudge") Boolean businessJudge,
             @QueryParam("uiId") String uiId,
             @QueryParam("piId") String piId) {
+        LOGGER.info("正在完成审批任务(id=" + tiId + ")");
         Map<String, Object> requiredData = new HashMap<String, Object>();
         requiredData.put("businessJudge",businessJudge);
-        if(uiId.equals("5") && businessJudge)
-            clientProcessService.changeProcessState(piId, ProcessInstanceState.PROCESS_INSTANCE_STATE_PASS);
-        if(uiId.equals("5") && !businessJudge)
-            clientProcessService.changeProcessState(piId, ProcessInstanceState.PROCESS_INSTANCE_STATE_FAIL);
-        clientTaskService.completeTask(tiId,pdId,userTaskNo,requiredData);
+        //TODO 这里 uiId=5 的是李总，建议后期维护时将其改为按照角色组别判断一票否决权
+        if(uiId.equals("5")) {
+            if (businessJudge) {
+                LOGGER.info("用户(id=" + uiId + ")直接通过了本次流程");
+                clientProcessService.changeProcessState(piId, ProcessInstanceState.PROCESS_INSTANCE_STATE_PASS);
+            } else {
+                LOGGER.info("用户(id=" + uiId + ")直接否决了本次流程");
+                clientProcessService.changeProcessState(piId, ProcessInstanceState.PROCESS_INSTANCE_STATE_FAIL);
+            }
+        }
+        if (clientTaskService.completeTask(tiId,pdId,userTaskNo,requiredData)) {
+            LOGGER.info("完成审批任务(id=" + tiId + ")成功");
+        } else {
+            LOGGER.error("完成审批任务(id=" + tiId + ")失败");
+        }
         return "1";
     }
 

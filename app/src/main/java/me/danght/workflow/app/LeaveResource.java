54c487b39b84ff937e85a18d252f4dc94d48119e
DangHT <dht925nerd@126.com>
2021-04-14 17:24:52 +0800

    为请假申请API添加日志

--- app/src/main/java/me/danght/workflow/app/LeaveResource.java
+++ app/src/main/java/me/danght/workflow/app/LeaveResource.java
@@ -5,10 +5,10 @@ import me.danght.workflow.app.dto.UserInfoDTO;
 import me.danght.workflow.app.service.LeaveInfoService;
 import me.danght.workflow.app.service.UserInfoService;
 import me.danght.workflow.common.api.form.ClientProcessService;
-import me.danght.workflow.common.api.form.ClientTaskService;
 import me.danght.workflow.common.constant.ParamType;
 import me.danght.workflow.common.dataobject.ParmObject;
 import org.apache.dubbo.config.annotation.DubboReference;
+import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
@@ -25,6 +25,8 @@ import java.util.Map;
 @Produces(MediaType.APPLICATION_JSON)
 public class LeaveResource {
 
+    private static final Logger LOGGER = Logger.getLogger(UserResource.class);
+
     @Inject
     UserInfoService userInfoService;
 
@@ -34,9 +36,6 @@ public class LeaveResource {
     @DubboReference(check = false)
     ClientProcessService clientProcessService;
 
-    @DubboReference(check = false)
-    ClientTaskService clientTaskService;
-
     @POST
     @Path("addLeave")
     @Consumes(MediaType.APPLICATION_JSON)
@@ -44,22 +43,38 @@ public class LeaveResource {
             @QueryParam("uiId") String uiId,
             @QueryParam("durations") String durations,
             @QueryParam("pdId") String pdId) {
+        LOGGER.info("用户(id=" + uiId + ")提交请假申请(id=" + pdId + "), 请假时长: " + durations + "天");
         LeaveInfoDTO leaveInfoDTO = new LeaveInfoDTO().setDurations(Integer.parseInt(durations))
                 .setUiId(uiId);
         leaveInfoDTO = leaveInfoService.addLeave(leaveInfoDTO);
+        if (leaveInfoDTO != null) {
+            LOGGER.info("用户(id=" + uiId + ")的请假申请(id=" + pdId + ")成功插入数据库");
+        } else {
+            LOGGER.error("用户(id=" + uiId + ")的请假申请(id=" + pdId + ")插入数据库失败");
+        }
         UserInfoDTO userInfoDTO = userInfoService.getUserById(uiId);
         Map<String, ParmObject> requiredData = new HashMap<>();
         ParmObject parmObject = new ParmObject()
                 .setPpType(ParamType.PARAM_TYPE_INT)
                 .setVal(Integer.parseInt(durations));
         requiredData.put("durations",parmObject);
-        clientProcessService.startProcess(pdId,userInfoDTO.getUiName() + "请假申请",userInfoDTO.getId(),leaveInfoDTO.getId(),requiredData);
+        if (clientProcessService.startProcess(
+                pdId,
+                userInfoDTO.getUiName() + "请假申请",
+                userInfoDTO.getId(),
+                leaveInfoDTO.getId(),
+                requiredData)) {
+            LOGGER.info("开启请假流程(id=" + leaveInfoDTO.getId() + ")成功");
+        } else {
+            LOGGER.error("开启请假流程(id=" + leaveInfoDTO.getId() + ")失败");
+        }
         return "1";
     }
 
     @GET
     @Path("queryLeaveInfo")
     public LeaveInfoDTO queryLeaveInfo(@QueryParam("piBusinessKey") String piBusinessKey) {
+        LOGGER.info("查询请假信息(piBusinessKey=" + piBusinessKey + ")");
         return leaveInfoService.getLeaveInfoById(piBusinessKey);
     }
 }
